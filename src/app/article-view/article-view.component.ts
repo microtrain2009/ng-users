c0da2c8e88439a529baee3975a177ba70a1b1f57
chad svastisalee <swasgrafx@gmail.com>
2020-11-16 09:36:42 -0600

    Implemented article view

--- src/app/article-view/article-view.component.ts
+++ src/app/article-view/article-view.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { ArticlesService } from '../articles.service';
+import { Article } from '../article';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-article-view',
@@ -7,9 +12,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ArticleViewComponent implements OnInit {
 
-  constructor() { }
+  article: Article;
 
-  ngOnInit(): void {
+  constructor(
+    private articlesService: ArticlesService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.getArticle(id);
   }
 
+  getArticle(id): void {
+    this.articlesService.getArticle(id).subscribe(
+      (response: any) => {
+        this.article = response.article
+      }
+    );
+  }
+  deleteArticle(id: string): void {
+    if(confirm("Are you sure to delete " + this.article.title)) {
+      this.articlesService.deleteArticle(id).subscribe(
+        ()=>{this.router.navigate(['/articles'])}
+      );
+    }
+  }
+  
+  
 }
